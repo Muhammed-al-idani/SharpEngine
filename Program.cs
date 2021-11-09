93cd49fc82f96a9367a328a5076ca7a8ba934b0b
Muhammed Al <muhammed.al-idani@student.forsbergsskola.se>
2021-11-09 14:07:18 +0100

    shader project

--- Program.cs
+++ Program.cs
@@ -9,6 +9,15 @@ namespace SharpEngine
         static void Main(string[] args)
         {
             Glfw.Init();
+            Glfw.WindowHint(Hint.ClientApi, ClientApi.OpenGL);
+            Glfw.WindowHint(Hint.ContextVersionMajor, 3);
+            Glfw.WindowHint(Hint.ContextVersionMinor, 3);
+            Glfw.WindowHint(Hint.Decorated, true);
+            Glfw.WindowHint(Hint.OpenglProfile, Profile.Core);
+            Glfw.WindowHint(Hint.OpenglForwardCompatible, Constants.True);
+            Glfw.WindowHint(Hint.Doublebuffer, Constants.False);
+            
+            
             var window = Glfw.CreateWindow(1024, 768, "SharpEngine", Monitor.None, Window.None);
             Glfw.MakeContextCurrent(window);
             Import(Glfw.GetProcAddress);
@@ -33,12 +42,48 @@ namespace SharpEngine
             }
             glEnableVertexAttribArray(0);
             
+            string vertexShaderSource = @"
+#version 330 core
+in vec3 pos;
+
+void main()
+{
+    gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);
+}
+";
+            string fragmentShaderSource = @"
+#version 330 core
+out vec4 result;
+
+void main()
+{
+    result = vec4(1, 0, 0, 1);
+}
+";
+            // create vertex shader
+            var vertexShader = glCreateShader(GL_VERTEX_SHADER);
+            glShaderSource(vertexShader, vertexShaderSource);
+            glCompileShader(vertexShader);
+            
+            //create fragment shader
+            
+            var fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+            glShaderSource(fragmentShader, fragmentShaderSource);
+            glCompileShader(fragmentShader);
+
+            var program = glCreateProgram();
+            glAttachShader(program,vertexShader);
+            glAttachShader(program, fragmentShader);
+            glLinkProgram(program);
+            glUseProgram(program);
+            
             
             while (!Glfw.WindowShouldClose(window))
             {
                 Glfw.PollEvents();// react to window changes .
                 glDrawArrays(GL_TRIANGLES,0,3);
                 Glfw.SwapBuffers(window);
+                glFlush();
             }
         }
     }
